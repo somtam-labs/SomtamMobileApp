1471a52e726780abcdb88bda9ee4d2a06b1aac9c
victoryeo <victoryeo1974@gmail.com>
2025-09-18 14:11:24 +0800

    add wallet page

---
+++ src/pages/wallet/Wallet.tsx
@@ -0,0 +1,13 @@
+import {
+  View,
+  ImageBackground,
+  StatusBar,
+} from 'react-native';
+
+const Wallet = ({ navigation }: any) => {
+    return (
+        <View style={{ flex: 1, marginTop: 20 }}></View>
+    )
+}
+
+export default Wallet;
